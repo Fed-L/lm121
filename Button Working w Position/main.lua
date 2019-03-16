1834606c5c568bfab318338cca3f95cd8023580d
Fed-L <47578825+Fed-L@users.noreply.github.com>
2019-03-16 17:12:54 +0000

    win32, button

---
+++ Button Working w Position/main.lua
@@ -0,0 +1,294 @@
+-----------------------------------------------------------------------------------------
+--
+-- main.lua
+--
+-----------------------------------------------------------------------------------------
+
+local physics = require( "physics" )
+physics.start()
+physics.setGravity( 0, 0 )
+ 
+-- Seed the random number generator
+math.randomseed( os.time() )
+
+-- Configure image sheet
+local sheetOptions =
+{
+    frames =
+    {
+        {   -- 1) asteroid 1
+            x = 0,
+            y = 0,
+            width = 102,
+            height = 85
+        },
+        {   -- 2) asteroid 2
+            x = 0,
+            y = 85,
+            width = 90,
+            height = 83
+        },
+        {   -- 3) asteroid 3
+            x = 0,
+            y = 168,
+            width = 100,
+            height = 97
+        },
+        {   -- 4) ship
+            x = 0,
+            y = 265,
+            width = 98,
+            height = 79
+        },
+        {   -- 5) laser
+            x = 98,
+            y = 265,
+            width = 14,
+            height = 40
+        },
+    },
+}
+local objectSheet = graphics.newImageSheet( "gameObjects.png", sheetOptions )
+
+-- Initialize variables
+local lives = 3
+local score = 0
+local died = false
+ 
+local asteroidsTable = {}
+ 
+local ship
+local gameLoopTimer
+local livesText
+local scoreText
+
+-- Set up display groups
+local backGroup = display.newGroup()  -- Display group for the background image
+local mainGroup = display.newGroup()  -- Display group for the ship, asteroids, lasers, etc.
+local uiGroup = display.newGroup()    -- Display group for UI objects like the score
+
+-- Load the background
+local background = display.newImageRect( backGroup, "background.jpg", 1024, 768 )
+background.x = display.contentCenterX
+background.y = display.contentCenterY
+
+ship = display.newImageRect( mainGroup, objectSheet, 4, 98, 79 )
+ship.x = display.contentCenterX
+ship.y = display.contentHeight - 100
+physics.addBody( ship, { radius=30, isSensor=true } )
+ship.myName = "ship"
+
+-- Display lives and score
+livesText = display.newText( uiGroup, "Lives: " .. lives, 200, 80, native.systemFont, 36 )
+scoreText = display.newText( uiGroup, "Score: " .. score, 400, 80, native.systemFont, 36 )
+
+-- Hide the status bar
+display.setStatusBar( display.HiddenStatusBar )
+
+
+local function updateText()
+    livesText.text = "Lives: " .. lives
+    scoreText.text = "Score: " .. score
+end
+
+
+local function createAsteroid()
+	local SPEED = 0.3
+	local newAsteroid = display.newImageRect( mainGroup, objectSheet, 1, 102, 85 )
+	table.insert( asteroidsTable, newAsteroid )
+	physics.addBody( newAsteroid, "dynamic", { radius=20, bounce=0.8 } )
+	newAsteroid.myName = "asteroid"
+
+	local whereFrom = math.random( 3 )
+
+	if ( whereFrom == 1 ) then
+		-- From the left
+		newAsteroid.x = -60
+		newAsteroid.y = math.random( 500 )
+		function gameLoop(event)
+			newAsteroid:setLinearVelocity(
+				SPEED * (ship.x - newAsteroid.x),
+				SPEED * (ship.y - newAsteroid.y)
+			)
+		end
+	elseif ( whereFrom == 2 ) then
+		-- From the top
+		newAsteroid.x = math.random( display.contentWidth )
+		newAsteroid.y = -60
+		function gameLoop(event)
+			newAsteroid:setLinearVelocity(
+				SPEED * (ship.x - newAsteroid.x),
+				SPEED * (ship.y - newAsteroid.y)
+			)
+		end
+	elseif ( whereFrom == 3 ) then
+		-- From the right
+		newAsteroid.x = display.contentWidth + 60
+		newAsteroid.y = math.random( 500 )
+		function gameLoop(event)
+			newAsteroid:setLinearVelocity(
+				SPEED * (ship.x - newAsteroid.x),
+				SPEED * (ship.y - newAsteroid.y)
+			)
+		end
+	end
+
+	newAsteroid:applyTorque( math.random( -6,6 ) )
+	Runtime:addEventListener("enterFrame", gameLoop)
+end
+
+	local speed=1000
+	local function shootBullet(event)
+		if event.phase=="began" then
+			local bullet=display.newRect(ship.x+20,ship.y,10,10)
+			bullet:setFillColor( 0,7,0 )
+			--camera:add(bullet,1,false)
+			bullet.isFixedRotation=true
+			deltaX = event.x - ship.x
+			deltaY = event.y - ship.y
+			angle = math.atan2( deltaY, deltaX )   --*180/math.pi (this turns radians to degrees and messed things up.)
+			physics.addBody( bullet)
+			bullet:setLinearVelocity( math.cos( angle )*speed , math.sin( angle )*speed  )
+		   end
+		end
+		Runtime:addEventListener("touch",shootBullet)
+
+
+local function dragShip( event )
+		if event.isPrimaryButtonDown then
+		 transition.to(ship, {x=event.x-6, y=event.y-7})
+		 angle = math.deg(math.atan2((event.y-ship.y),(event.x-ship.x)))
+		 ship.rotation = angle + 90
+		else
+		end
+	end
+	 
+Runtime:addEventListener('mouse',dragShip) 
+
+
+
+
+local function gameLoop()
+
+	-- Create new asteroid
+	createAsteroid()
+
+	-- Remove asteroids which have drifted off screen
+	for i = #asteroidsTable, 1, -1 do
+		local thisAsteroid = asteroidsTable[i]
+
+		if ( thisAsteroid.x < -100 or
+			 thisAsteroid.x > display.contentWidth + 100 or
+			 thisAsteroid.y < -100 or
+			 thisAsteroid.y > display.contentHeight + 100 )
+		then
+			display.remove( thisAsteroid )
+			table.remove( asteroidsTable, i )
+		end
+	end
+end
+
+gameLoopTimer = timer.performWithDelay( 500, gameLoop, 0 )
+
+
+local function restoreShip()
+
+	ship.isBodyActive = false
+	ship.x = display.contentCenterX
+	ship.y = display.contentHeight - 100
+
+	-- Fade in the ship
+	transition.to( ship, { alpha=1, time=4000,
+		onComplete = function()
+			ship.isBodyActive = true
+			died = false
+		end
+	} )
+end
+
+
+local function onCollision( event )
+
+	if ( event.phase == "began" ) then
+
+		local obj1 = event.object1
+		local obj2 = event.object2
+
+		if ( ( obj1.myName == "laser" and obj2.myName == "asteroid" ) or
+			 ( obj1.myName == "asteroid" and obj2.myName == "laser" ) )
+		then
+			-- Remove both the laser and asteroid
+			display.remove( obj1 )
+			display.remove( obj2 )
+
+			for i = #asteroidsTable, 1, -1 do
+				if ( asteroidsTable[i] == obj1 or asteroidsTable[i] == obj2 ) then
+					table.remove( asteroidsTable, i )
+					break
+				end
+			end
+
+			-- Increase score
+			score = score + 100
+			scoreText.text = "Score: " .. score
+
+		elseif ( ( obj1.myName == "ship" and obj2.myName == "asteroid" ) or
+				 ( obj1.myName == "asteroid" and obj2.myName == "ship" ) )
+		then
+			if ( died == false ) then
+				died = true
+
+				-- Update lives
+				lives = lives - 1
+				livesText.text = "Lives: " .. lives
+
+				if ( lives == 0 ) then
+					display.remove( ship )
+				else
+					ship.alpha = 0
+					timer.performWithDelay( 1000, restoreShip )
+				end
+			end
+		end
+	end
+end
+
+Runtime:addEventListener( "collision", onCollision )
+
+--
+--Sliding Inventory
+--
+local widget = require( "widget" )
+ 
+-- Function to handle button events
+local function handleButtonEvent( event )
+ 
+    if ( "ended" == event.phase ) then
+        print( "Button was pressed and released" )
+    end
+end
+ 
+local button1 = widget.newButton(
+    {
+        width = 240,
+        height = 120,
+        defaultFile = "buttonDefault.png",
+        overFile = "buttonOver.png",
+        label = "button",
+        onEvent = handleButtonEvent
+    }
+)
+ 
+-- Positioning the button
+local centerX = display.contentCenterX
+local centerY = display.contentCenterY
+local screenTop = display.screenOriginY
+local screenLeft = display.screenOriginX
+local screenBot = display.contentHeight - display.screenOriginY
+local screenRight = display.contentWidth - display.screenOriginX
+
+button1.x = screenLeft + 150
+button1.y = screenTop + 100
+ 
+-- Change the button's label text
+button1:setLabel( "2-Image" )
